0bc714d30445e28bcf553f63214fb889181b1176
MaximFirsov01 <165218680+MaximFirsov01@users.noreply.github.com>
2024-04-08 12:14:22 -0700

    upload - Premiere OnScript Template
    This is an outdated template, does not support debugging in vscode.

---
+++ Premiere OnScript Template/jsx/extendScript.jsx
@@ -0,0 +1,12 @@
+
+$.runScript = {
+
+
+	changeThisFunctionName: function() {
+	//--------------------------------------------------------------------------------------------------------------------------------------------------
+		//Copy/Paste your script from Adobe Extendscript HERE!!!!
+		alert("Please find and read the ReadMe.txt file for instruction on how to use this panel template.")
+	//--------------------------------------------------------------------------------------------------------------------------------------------------
+}	
+    
+}
